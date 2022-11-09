a98c15dd4f69b96b79035bf2e37cd4ba6eccd65b
Austin Griffin <austintg94@gmail.com>
2022-11-08 19:51:21 -0700

    commit tree 500ccad6de8eb560a6c298cb036c87f6a5f4d32e

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+function appendCat(name)
+{
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+function prependCat(name)
+{
+    const copyOfCats2 = [name, ...cats];
+    return copyOfCats2;
+}
+function removeLastCat()
+{
+    const copyOfCats3 = cats.slice(0, 2);
+    return copyOfCats3;
+}
+function removeFirstCat()
+{
+    const copyOfCats4 = cats.slice(1, 3);
+    return copyOfCats4;
+}
